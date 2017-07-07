cfcc60f5efb6c11a8cc241c21299c890309052f7
00nyrhtak <jenny91079@gmail.com>
2017-07-07 19:09:14 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(".menu-btn").click(
+  function(){
+    $("#overlay").css("top","0");
+  }
+);
+
+$(".close-btn").click(
+  function(){
+    $("#overlay").css("top","-1000%");
+  }
+);
